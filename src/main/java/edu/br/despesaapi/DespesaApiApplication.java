485f7702efb32eac7b3fd11b4fa603981333c8ea
Breno Lima <85833737+BrenoLim4@users.noreply.github.com>
2023-08-02 17:17:40 -0300

    first commit

---
+++ src/main/java/edu/br/despesaapi/DespesaApiApplication.java
@@ -0,0 +1,13 @@
+package edu.br.despesaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/br/despesaapi/DespesaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.br.despesaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
